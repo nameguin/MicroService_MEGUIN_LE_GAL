d3b7e86691608300702c5c2454893ecc790f243d
Nathan MEGUIN <nathan.meguin@etu.uca.fr>
2024-01-21 20:19:17 +0100

    Fin merge

--- Front/Services/TaskService.cs
+++ Front/Services/TaskService.cs
@@ -51,13 +51,6 @@ namespace Front.Services
 
         public async Task<(TaskModel? task, string? error)> CreateTask(string title, string? description, DateTime deadline)
         {
-            var task = new TaskCreateModel() {
-                IsDone = false,
-                Title = title,
-                Description = description,
-                Deadline = deadline,
-
-            };
             var jwt = await _sessionStorage.GetAsync<string>("jwt");
             var token = await _sessionStorage.GetAsync<string>("jwt");
 
@@ -65,63 +58,61 @@ namespace Front.Services
             {
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
 
-                var task = new TaskCreateModel() { IsDone = false, Text = "Empty" };
+                var task = new TaskCreateModel()
+                {
+                    IsDone = false,
+                    Title = title,
+                    Description = description,
+                    Deadline = deadline,
+
+                };
                 HttpResponseMessage response = await _httpClient.PostAsJsonAsync("http://localhost:5000/api/Task/create", task);
 
-            if (response.IsSuccessStatusCode)
-            {
-                var result = await response.Content.ReadFromJsonAsync<TaskModel>();
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadFromJsonAsync<TaskModel>();
 
-                return (result,"");
+                    return (result, "");
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    return (null, error);
+                }
             }
-            else
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                return (null, error);
-            }
+            return (null, "Invalid Token");
         }
 
         public async Task<(TaskModel? task, string? error)> UpdateTask(TaskModel todo)
         {
-            var task = new TaskCreateModel() { IsDone = todo.IsDone, Title = todo.Title, Description = todo.Description, Deadline = todo.Deadline };
-
             var token = await _sessionStorage.GetAsync<string>("jwt");
 
             if (token.Success)
             {
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
 
-                var task = new TaskCreateModel() { IsDone = todo.IsDone, Text = todo.Text };
+                var task = new TaskCreateModel() { IsDone = todo.IsDone, Title = todo.Title, Description = todo.Description, Deadline = todo.Deadline };
 
-            var jwt = await _sessionStorage.GetAsync<string>("jwt");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt.Value);
-            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"http://localhost:5000/api/Task/update/{todo.Id}", task);
-            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"http://localhost:5000/api/Task/update/{todo.Id}", task);
+                var jwt = await _sessionStorage.GetAsync<string>("jwt");
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt.Value);
+                HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"http://localhost:5000/api/Task/update/{todo.Id}", task);
 
                 Console.WriteLine(response.Content.ToString());
                 Console.WriteLine(response.StatusCode);
+
                 if (response.IsSuccessStatusCode)
                 {
                     var result = await response.Content.ReadFromJsonAsync<TaskModel>();
-                return (result, "");
-            }
-            else
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                return (null, error);
-                    return result;
+                    return (result, "");
                 }
                 else
                 {
-                    return null;
+                    var error = await response.Content.ReadAsStringAsync();
+                    return (null, error);
                 }
             }
-            else
-            {
-                Console.WriteLine("Erreur : Token JWT impossible à récupérer");
-                return null;
-                return null;
-            }
+            Console.WriteLine("Erreur : Token JWT impossible à récupérer");
+            return (null, "Invalid Token");
         }
 
         public async Task DeleteTask(int id)
